b8986ff844b22bad7422c25d0ff8b7c17bbf54d7
Olivier Pelgrin <olivier@cs.aau.dk>
2023-03-21 15:18:59 +0000

    update index.ts to export the new classes

--- lib/index.ts
+++ lib/index.ts
@@ -1,3 +1,5 @@
 export * from './IOstrichStoreNative';
 export * from './OstrichStore';
 export * from './utils';
+export * from './IBufferedOstrichStoreNative';
+export * from './BufferedOstrichStore';
